52a7afb91161f64d175f922199cf4a7210e8d3e6
den19okn <160195979+den19okn@users.noreply.github.com>
2024-02-16 10:46:47 +0900

    Add files via upload

---
+++ _htdocsのコピー/js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+  $(".slider").slick({
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    centerMode: false,
+    centerPadding: "0",
+    dots: false,
+    // fade: true,
+    slidesToShow: 1,
+    speed: 300,
+  });
+});
